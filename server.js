3cd291fd4362eda1beb2774aeabdcba3d1aad6c6
Fer-2202 <imarin.marin2003fwd@gmail.com>
2025-08-18 10:46:03 -0600

    correción 3 al server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-const fetch = require('node-fetch');
 
 const app = express();
 
+// Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta principal
@@ -11,11 +11,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'pages/index.html'));
 });
 
+// Escuchar en el puerto 3000
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
-
-server.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
 });
