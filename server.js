ae8495f2898d195a76efce1861c5f98a148318ff
Fer-2202 <imarin.marin2003fwd@gmail.com>
2025-08-18 10:32:45 -0600

    correción al server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('¡Hola desde Node.js!');
+    const filePath = path.join(__dirname, 'public/pages/index.html');
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Error al cargar el archivo HTML');
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, () => {
